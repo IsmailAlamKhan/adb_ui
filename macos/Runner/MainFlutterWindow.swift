c88ad7d192e57cb9dfbdcd0855f099022e846a66
IsmailAlamKhan <mdismailalamkhan@gmail.com>
2022-11-07 20:01:17 +0600

    recreate mac os project

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,6 +1,6 @@
 import Cocoa
 import FlutterMacOS
-import window_manager
+
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
@@ -12,9 +12,4 @@ class MainFlutterWindow: NSWindow {
 
     super.awakeFromNib()
   }
-
-  override public func order(_ place: NSWindow.OrderingMode, relativeTo otherWin: Int) {
-    super.order(place, relativeTo: otherWin)
-//    hiddenWindowAtLaunch()
-  }  
 }
